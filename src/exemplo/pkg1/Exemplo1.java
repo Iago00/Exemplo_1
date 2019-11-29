f8cd682a6582cde302299495f9cd0462b4640039
ipampillonvieitez <ipampillonvieitez@danielcastelao.org>
2019-11-29 12:40:33 +0100

    Inicio

---
+++ src/exemplo/pkg1/Exemplo1.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo.pkg1;
+
+/**
+ *
+ * @author ipampillonvieitez
+ */
+public class Exemplo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // calcula lonxitude circunferencia
+        /*float radio = 2.5f, lonxitude;
+        lonxitude = 2*3.14f*radio; 
+        System.out.println("lonxitude = "+ lonxitude);*/
+        
+        /*float radio = 2.5f;
+        System.out.println("lonxitude = "+ (2*3.14*radio));*/
+       /*float radio = 2.5f;
+        float lonxitude = 2*3.14f*radio;
+        System.out.println("lonxitude = "+ lonxitude);*/
+        //utilizando a constante PI 
+        /*float radio = 2.5f;
+        final float PI = 3.14f;
+        System.out.println("lonxitude = "+ (2*PI*radio));*/
+        //utilizando a clase Match
+        /*float radio = 2.5f;
+        float lonxitude = (float) (2*Math.PI*radio);*/
+        Scanner sc = new Scanner(System.int);
+
+        float radio = Sc.nextFloat()
+        System.out.
+        System.out.println("lonxitude ="+ (2*Math.PI*radio));
+
+            
+        })
+        
+        
+
+
+
+        
+        
+        
+    }
+    
+}
